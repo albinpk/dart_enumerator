1923788c60ec76233773d6eaa6bb92789331b874
Albin <56157868+albinpk@users.noreply.github.com>
2026-01-31 19:23:22 +0530

    feat: create workspace

---
+++ packages/enumerator/test/enumerator_test.dart
@@ -0,0 +1,16 @@
+import 'package:enumerator/enumerator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
---
+++ packages/enumerator_builder/example/enumerator_builder_example.dart
@@ -0,0 +1,6 @@
+import 'package:enumerator_builder/enumerator_builder.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
